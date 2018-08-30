1dbb9a87f817d95c4b2531f010f2be0977e5c0ae
SriKrishna Paparaju <42730908+SriKrishnaPaparaju@users.noreply.github.com>
2018-08-30 20:34:01 +0530

    Update stack-analysis.sh

--- stack-analysis.sh
+++ stack-analysis.sh
@@ -1,8 +1,8 @@
 
  
  echo "Generating effective POM.."
- cp /projects/web-java-spring-project/pom.xml /projects/web-java-spring-project/epom.xml
- mvn help:effective-pom -f  "/projects/web-java-spring-project/epom.xml" -Doutput="/projects/web-java-spring-project/pom.xml" &> /projects/vertx-http/outfile
+ cp /projects/vertx-http/pom.xml /projects/vertx-http/epom.xml
+ mvn help:effective-pom -f  "/projects/vertx-http/epom.xml" -Doutput="/projectsvertx-http/pom.xml" &> /projects/vertx-http/outfile
  echo "Analyzing your application stack.."
  
-curl -X POST    -F 'manifest=@/projects/web-java-spring-project/pom.xml' 192.168.225.126:7000/stack-analysis 2> /projects/vertx-http/errorlog
+curl -X POST    -F 'manifest=@/projects/vertx-http/pom.xml' 192.168.225.126:7000/stack-analysis 2> /projects/vertx-http/errorlog
