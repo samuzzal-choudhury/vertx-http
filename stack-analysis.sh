83d8f52b03426278829b630878edd7f7baa92ed7
SriKrishna Paparaju <42730908+SriKrishnaPaparaju@users.noreply.github.com>
2018-08-30 17:09:11 +0530

    Create stack-analysis.sh

---
+++ stack-analysis.sh
@@ -0,0 +1,8 @@
+
+ 
+ echo "Generating effective POM.."
+ cp /projects/web-java-spring-project/pom.xml /projects/web-java-spring-project/epom.xml
+ mvn help:effective-pom -f  "/projects/web-java-spring-project/epom.xml" -Doutput="/projects/web-java-spring-project/pom.xml" &> outfile
+ echo "Analyzing your application stack.."
+ 
+curl -X POST    -F 'manifest=@/projects/web-java-spring-project/pom.xml' 192.168.225.126:7000/stack-analysis
