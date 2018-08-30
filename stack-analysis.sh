0e8fb85a9353260b3a1b8b102e9f6bf9d7ab9423
SriKrishna Paparaju <42730908+SriKrishnaPaparaju@users.noreply.github.com>
2018-08-30 20:50:09 +0530

    Update stack-analysis.sh

--- stack-analysis.sh
+++ stack-analysis.sh
@@ -4,4 +4,5 @@
  mvn help:effective-pom -f  "/projects/vertx-http/epom.xml" -Doutput="/projectsvertx-http/pom.xml" &> /projects/vertx-http/outfile
  echo "Analyzing your application stack.."
  
+ 
 curl -X POST    -F 'manifest=@/projects/vertx-http/pom.xml' 192.168.225.126:7000/stack-analysis 2> /projects/vertx-http/errorlog
