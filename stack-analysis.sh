1dfcc41835bdd819153941f7239d43209f295b9a
Samuzzal Choudhury <samuzzal@redhat.com>
2018-10-30 09:01:32 +0000

    modified scripts

--- stack-analysis.sh
+++ stack-analysis.sh
@@ -1,8 +1,57 @@
 
- 
- echo "Generating effective POM.."
- mvn help:effective-pom -f  "/projects/vertx-http/epom.xml" -Doutput="/projectsvertx-http/pom.xml" &> /projects/vertx-http/outfile
- echo "Analyzing your application stack.."
- 
- 
-curl -X POST    -F 'manifest=@/projects/vertx-http/pom.xml' 23.251.159.157:7000/stack-analysis 2> /projects/vertx-http/errorlog
+#!/bin/bash
+
+usage() {
+    echo "Usage:"
+    echo "$0 [-f <manifest filename with path>][-p <project directory>]"
+    exit -1
+}
+
+while getopts ":f:p:" o; do
+    case "${o}" in
+        f)
+            f=${OPTARG}
+            ;;
+        p)
+            p=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+#if [ -z "${f}" ] || [ -z "${p}" ]; then
+#    usage
+#fi
+
+f='/projects/vertx-http/pom.xml'
+p='/home/sam'
+
+echo "Analyzing your application stack ..."
+
+manifest=$(basename $f)
+if [ "$manifest" == "pom.xml" ]
+then
+    echo "Generating effective POM.."
+    # mvn help:effective-pom -f  "/tmp/epom.xml" -Doutput="$f" &> /projects/vertx-http/outfile
+    mkdir -p /tmp/target
+    # cp -f /tmp/epom.xml /tmp/target/pom.xml
+    manifest='./target/pom.xml'
+else
+    manifest=$f
+fi
+
+# Environment Variables to be used for user_key and api_url
+user_key='250f7573417ff52aee50728f698ecd96'
+api_url='https://friendly_system_service-2445582075730.production.gw.apicast.io:443/api/v1/stack-analyses/'
+
+manifest='@'$manifest
+output=`curl -s -X POST -H"user-key: $user_key" -F "manifest[]=$manifest" -F"filePath[]=$p"  $api_url`
+id=`echo $output|python -c "import sys, json; print(json.load(sys.stdin)['id'])"`
+
+sleep 5
+
+get_cmd="$api_url/$id"
+curl -H"user-key: $user_key" -s $get_cmd | sed 's/"//g' | sed 's/\\t/    /g' | sed 's/\\n/ \
+/g'
